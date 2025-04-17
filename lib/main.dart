3534464758964161854e0829e2f4d1e3aabffb51
Akash Morwal <akashbhau101@gmail.com>
2025-04-17 09:02:05 +0530

    New Package of Flutter are intl Package are used to formate the datetime

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,9 +16,10 @@ class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    var arrNames = ["Ramesh", "Suresh", "ganesh", "Mahesh"];
-    var emailText = TextEditingController();
-    var passText = TextEditingController();
+    // var arrNames = ["Ramesh", "Suresh", "ganesh", "Mahesh"];
+    // var emailText = TextEditingController();
+    // var passText = TextEditingController();
+    var time = DateTime.now();
 
     return MaterialApp(
       theme: ThemeData(),
@@ -35,104 +37,133 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Center(
           child: Container(
-            width: 300,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Sign in",style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
-                Container(height: 20,),
-                TextField(
-                  controller: emailText,
-                  decoration: InputDecoration(
-                    hintText: "Enter Email",
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        color: Colors.pink,
-                            width: 3,
-                        style: BorderStyle.solid
-
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        color: Colors.blue,
-                        width: 3,
-                        style: BorderStyle.solid
-                      )
-                    ),
-                    disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        color: Colors.grey,
-                        width: 3,
-                        style: BorderStyle.solid
-                      )
-                    ),
-                    suffixIcon: Icon(Icons.email),prefixIconColor: Colors.pink,
-                  ),
-                ),
-                Container(height: 15,),
-                TextField(
-                  controller: passText,
-                  obscureText: true,
-                  // obscuringCharacter: '*',
-                  decoration: InputDecoration(
-                    hintText: "Enter Password",
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                            color: Colors.pink,
-                            width: 3,
-                            style: BorderStyle.solid
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(
-                              color: Colors.blue,
-                              width: 3,
-                              style: BorderStyle.solid
-                          )
-                      ),
-                      disabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(
-                              color: Colors.grey,
-                              width: 3,
-                              style: BorderStyle.solid
-                          )
-                      ),
-                    suffixIcon: IconButton(onPressed: (){}, icon: Icon(Icons.remove_red_eye)),
-
-                  ),
-
-
-                ),
-                Container(height: 20,),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    // shape: StadiumBorder(),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    shadowColor: Colors.black,
-                    backgroundColor: Colors.blue,
-                  ),
-                  onPressed: (){
-                  String uEmail = emailText.text.toString();
-                  String uPass = passText.text.toString();
-                  print("Email : $uEmail, Password :  $uPass" );
-                }, child: Text("Login",style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 22),),),
-              ],
-            ),
-          ),
-        )
+              width: 200,
+              height: 200,
+              padding: EdgeInsets.all(11),
+              color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Today: ${DateFormat('yMMMMEEEEd').format(time)}',style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+                  // ElevatedButton(onPressed: (){
+                  //   setState(() {
+                  //
+                  //   });
+                  // }, child: Text('Get DateTime'))
+                ],
+              )),
+        ),
       ),
     );
   }
 }
+
+
+
+//Login Page Ui
+
+//
+// Center(
+// child: Container(
+// width: 300,
+// child: Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: [
+// Text("Sign in",style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
+// Container(height: 20,),
+// TextField(
+// keyboardType: TextInputType.emailAddress,
+// controller: emailText,
+// decoration: InputDecoration(
+// hintText: "Enter Email",
+// focusedBorder: OutlineInputBorder(
+// borderRadius: BorderRadius.circular(10),
+// borderSide: BorderSide(
+// color: Colors.pink,
+// width: 3,
+// style: BorderStyle.solid
+//
+// ),
+// ),
+// enabledBorder: OutlineInputBorder(
+// borderRadius: BorderRadius.circular(10),
+// borderSide: BorderSide(
+// color: Colors.blue,
+// width: 3,
+// style: BorderStyle.solid
+// )
+// ),
+// disabledBorder: OutlineInputBorder(
+// borderRadius: BorderRadius.circular(10),
+// borderSide: BorderSide(
+// color: Colors.grey,
+// width: 3,
+// style: BorderStyle.solid
+// )
+// ),
+// suffixIcon: Icon(Icons.email),prefixIconColor: Colors.pink,
+// ),
+// ),
+// Container(height: 15,),
+// TextField(
+// controller: passText,
+// obscureText: true,
+// // obscuringCharacter: '*',
+// decoration: InputDecoration(
+// hintText: "Enter Password",
+// focusedBorder: OutlineInputBorder(
+// borderRadius: BorderRadius.circular(10),
+// borderSide: BorderSide(
+// color: Colors.pink,
+// width: 3,
+// style: BorderStyle.solid
+// ),
+// ),
+// enabledBorder: OutlineInputBorder(
+// borderRadius: BorderRadius.circular(10),
+// borderSide: BorderSide(
+// color: Colors.blue,
+// width: 3,
+// style: BorderStyle.solid
+// )
+// ),
+// disabledBorder: OutlineInputBorder(
+// borderRadius: BorderRadius.circular(10),
+// borderSide: BorderSide(
+// color: Colors.grey,
+// width: 3,
+// style: BorderStyle.solid
+// )
+// ),
+// suffixIcon: IconButton(onPressed: (){}, icon: Icon(Icons.remove_red_eye)),
+//
+// ),
+//
+//
+// ),
+// Container(height: 20,),
+// ElevatedButton(
+// style: ElevatedButton.styleFrom(
+// // shape: StadiumBorder(),
+// shape: RoundedRectangleBorder(
+// borderRadius: BorderRadius.circular(10),
+// ),
+// shadowColor: Colors.black,
+// backgroundColor: Colors.blue,
+// ),
+// onPressed: (){
+// String uEmail = emailText.text.toString();
+// String uPass = passText.text.toString();
+// print("Email : $uEmail, Password :  $uPass" );
+//
+// }, child: Text("Login",style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 22),),),
+// ],
+// ),
+// ),
+// )
+
+
+
 
 
 // Center(
